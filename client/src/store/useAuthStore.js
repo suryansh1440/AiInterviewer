d5b930684a5e4827aa82a177d163c8277fe70183
Divanshi5826 <divanshikathuria5826@gmail.com>
2025-07-17 20:45:58 +0530

    adding store to Start page

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,23 +1,21 @@
 import { create } from "zustand";
 
 export const useAuthStore = create((set) => ({
-    // user: null ,
-   user: {
-    _id:4372894274162791,
+  // user: null,
+  user: {
+    _id: 4372894274162791,
     name: "Ankit",
     email: "suryansh1440@gmail.com",
-    role:"ADMIN",
+    role: "ADMIN",
     profilePic: "https://randomuser.me/api/portraits/men/75.jpg",
     phone: "+91 3216549870",
     createdAt: "01-01-25",
     lastLogin: "02-03-23",
     level: 1,
-    interviewLeft:10,
+    interviewLeft: 10,
     stats: {
       totalInterviews: 12,
       averageScore: "87%",
-    }
-    
-  },
-
-}))
+    },
+  }
+}));
