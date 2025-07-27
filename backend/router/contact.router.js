a1b77c22900d419e41e71da1f3ae4e42e1e00cc4
Harsh Singh <harsh.singh.2003dydt@gmail.com>
2025-07-27 20:25:23 +0530

    added route protection middleware

--- backend/router/contact.router.js
+++ backend/router/contact.router.js
@@ -1,8 +1,9 @@
 import express from "express";
 import getUserContacts from "../controller/contact.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const contactRouter = express.Router();
 
-contactRouter.route("/").post(getUserContacts);
+contactRouter.route("/").post(protectRoute, getUserContacts);
 
 export default contactRouter;
